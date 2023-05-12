88ecf177a1f95496a113283480661e321e88dd74
Anurag Dhote <anuragdhote392@gmail.com>
2023-05-13 04:30:33 +0530

    feat - added some products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import Image from 'next/image'
 
-export default function Page() {
+import product1Img from './assets/product-1.jpg'
+import product2Img from './assets/product-2.jpg'
+import product3Img from './assets/product-3.jpg'
+
+import Carousel from './Carousel'
+
+type Props = {}
+
+export default function page({ }: Props) {
     return (
-        <div>Dashboard</div>
+        <div className="min-w-full px-20 text-white font-display">
+            <h1 className='text-4xl my-5 text-center'>Our Products</h1>
+            <div className='my-5'>
+                <Image
+                    src={product1Img}
+                    alt='Product 1'
+                />
+            </div>
+            <div className='my-5'>
+                <Image
+                    src={product2Img}
+                    alt='Product 2'
+                />
+            </div>
+            <div className='my-5'>
+                <Image
+                    src={product3Img}
+                    alt='Product 3'
+                />
+            </div>
+            <Carousel />
+        </div>
     )
 }
